8b2e83ac48ef44e6fa7c99eade6f4ce0145bc8d7
Domi <dominikasulot@gmail.com>
2017-10-03 23:27:33 +0200

    one-way sorting added, revert sort TODO

--- src/app/components/search/searchrecord/searchrecord.component.ts
+++ src/app/components/search/searchrecord/searchrecord.component.ts
@@ -10,6 +10,39 @@ export class SearchrecordComponent implements OnInit {
     { id: 2, name: 'karafjka', surname: 'kowalska', postcode: '50-235', city: 'Wroclaw', street: 'wawrzynkowskiego', number: 20, flatNumber: 90},
     { id: 8, name: 'anna', surname: 'towalska', postcode: '60-235', city: 'Krakow', street: 'pilsudzkiego', number: 14, flatNumber: 12 },
     { id: 3, name: 'snna', surname: 'powalska', postcode: '70-235', city: 'Antonin', street: 'lukasinskiego', number: 8, flatNumber: 43 },
+    { id: 2, name: 'karafjka', surname: 'kowaelska', postcode: '50-245', city: 'Wroclaw', street: 'wawrzynkowskiego', number: 20, flatNumber: 90},
+    { id: 89, name: 'asnna', surname: 'towalseka', postcode: '60-245', city: 'Krakow', street: 'pilsudzriego', number: 14, flatNumber: 12 },
+    { id: 36, name: 'snsna', surname: 'powalseka', postcode: '70-245', city: 'Antorin', street: 'lukasirskiego', number: 8, flatNumber: 43 },
+    { id: 25, name: 'karsafjka', surname: 'kowealska', postcode: '50-245', city: 'Wgoclaw', street: 'warrzynkowskiego', number: 20, flatNumber: 90},
+    { id: 82, name: 'annsa', surname: 'towalskea', postcode: '60-245', city: 'Krasow', street: 'pilsudzriego', number: 14, flatNumber: 12 },
+    { id: 32, name: 'snsna', surname: 'powalskea', postcode: '73-235', city: 'Antosin', street: 'lukasirskiego', number: 8, flatNumber: 43 },
+    { id: 23, name: 'karsafjka', surname: 'kowealska', postcode: '53-235', city: 'Wsoclaw', street: 'warrzynkowskiego', number: 20, flatNumber: 90},
+    { id: 85, name: 'annsa', surname: 'towealska', postcode: '60-235', city: 'Kradkow', street: 'pilsudrkiego', number: 14, flatNumber: 12 },
+    { id: 34, name: 'snnsa', surname: 'peowalska', postcode: '73-235', city: 'Anhtonin', street: 'lukasrnskiego', number: 8, flatNumber: 43 },
+    { id: 2, name: 'karafjka', surname: 'kowalska', postcode: '50-235', city: 'Wroclaw', street: 'wawrzynkowskiego', number: 20, flatNumber: 90},
+    { id: 8, name: 'anna', surname: 'towalska', postcode: '60-235', city: 'Krakow', street: 'pilsudzkiego', number: 14, flatNumber: 12 },
+    { id: 3, name: 'snna', surname: 'powalska', postcode: '70-235', city: 'Antonin', street: 'lukasinskiego', number: 8, flatNumber: 43 },
+    { id: 2, name: 'karafjka', surname: 'kowaelska', postcode: '50-245', city: 'Wroclaw', street: 'wawrzynkowskiego', number: 20, flatNumber: 90},
+    { id: 89, name: 'asnna', surname: 'towalseka', postcode: '60-245', city: 'Krakow', street: 'pilsudzriego', number: 14, flatNumber: 12 },
+    { id: 36, name: 'snsna', surname: 'powalseka', postcode: '70-245', city: 'Antorin', street: 'lukasirskiego', number: 8, flatNumber: 43 },
+    { id: 25, name: 'karsafjka', surname: 'kowealska', postcode: '50-245', city: 'Wgoclaw', street: 'warrzynkowskiego', number: 20, flatNumber: 90},
+    { id: 82, name: 'annsa', surname: 'towalskea', postcode: '60-245', city: 'Krasow', street: 'pilsudzriego', number: 14, flatNumber: 12 },
+    { id: 32, name: 'snsna', surname: 'powalskea', postcode: '73-235', city: 'Antosin', street: 'lukasirskiego', number: 8, flatNumber: 43 },
+    { id: 23, name: 'karsafjka', surname: 'kowealska', postcode: '53-235', city: 'Wsoclaw', street: 'warrzynkowskiego', number: 20, flatNumber: 90},
+    { id: 85, name: 'annsa', surname: 'towealska', postcode: '60-235', city: 'Kradkow', street: 'pilsudrkiego', number: 14, flatNumber: 12 },
+    { id: 34, name: 'snnsa', surname: 'peowalska', postcode: '73-235', city: 'Anhtonin', street: 'lukasrnskiego', number: 8, flatNumber: 43 },
+    { id: 2, name: 'karafjka', surname: 'kowalska', postcode: '50-235', city: 'Wroclaw', street: 'wawrzynkowskiego', number: 20, flatNumber: 90},
+    { id: 8, name: 'anna', surname: 'towalska', postcode: '60-235', city: 'Krakow', street: 'pilsudzkiego', number: 14, flatNumber: 12 },
+    { id: 3, name: 'snna', surname: 'powalska', postcode: '70-235', city: 'Antonin', street: 'lukasinskiego', number: 8, flatNumber: 43 },
+    { id: 2, name: 'karafjka', surname: 'kowaelska', postcode: '50-245', city: 'Wroclaw', street: 'wawrzynkowskiego', number: 20, flatNumber: 90},
+    { id: 89, name: 'asnna', surname: 'towalseka', postcode: '60-245', city: 'Krakow', street: 'pilsudzriego', number: 14, flatNumber: 12 },
+    { id: 36, name: 'snsna', surname: 'powalseka', postcode: '70-245', city: 'Antorin', street: 'lukasirskiego', number: 8, flatNumber: 43 },
+    { id: 25, name: 'karsafjka', surname: 'kowealska', postcode: '50-245', city: 'Wgoclaw', street: 'warrzynkowskiego', number: 20, flatNumber: 90},
+    { id: 82, name: 'annsa', surname: 'towalskea', postcode: '60-245', city: 'Krasow', street: 'pilsudzriego', number: 14, flatNumber: 12 },
+    { id: 32, name: 'snsna', surname: 'powalskea', postcode: '73-235', city: 'Antosin', street: 'lukasirskiego', number: 8, flatNumber: 43 },
+    { id: 23, name: 'karsafjka', surname: 'kowealska', postcode: '53-235', city: 'Wsoclaw', street: 'warrzynkowskiego', number: 20, flatNumber: 90},
+    { id: 85, name: 'annsa', surname: 'towealska', postcode: '60-235', city: 'Kradkow', street: 'pilsudrkiego', number: 14, flatNumber: 12 },
+    { id: 34, name: 'snnsa', surname: 'peowalska', postcode: '73-235', city: 'Anhtonin', street: 'lukasrnskiego', number: 8, flatNumber: 43 },
   ];
 
 
@@ -22,6 +55,11 @@ export class SearchrecordComponent implements OnInit {
     this.users.sort((a, b) => a.id - b.id);
     console.log('clicked');
   }
+
+ /* revertSortById(){
+    this.users.sort((a, b) => b.id - a.id);
+    console.log('revert-clicked');
+  }*/
 
   sortByName() {
     this.users.sort((a, b) => { if (a.name < b.name) { return -1; }
@@ -64,6 +102,10 @@ export class SearchrecordComponent implements OnInit {
 
   sortByFlatNumber() {
     this.users.sort((a, b) => a.flatNumber - b.flatNumber);
+  }
+
+  search() {
+    console.log( 'searchClicked' );
   }
 }
 

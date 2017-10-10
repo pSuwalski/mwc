af9ca550bdbe98390cde2cfc506c4a871fc459d6
Domi <dominikasulot@gmail.com>
2017-10-10 23:38:57 +0200

    data sorting added

--- src/app/components/search/searchrecord/searchrecord.component.ts
+++ src/app/components/search/searchrecord/searchrecord.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {stringDistance} from "codelyzer/util/utils";
 import { Router } from '@angular/router';
+import {MatHeaderRowDef} from '@angular/material';
+import {Sort} from '@angular/material';
 
 @Component({
   selector: 'mwc-searchrecord',
@@ -12,7 +13,7 @@ export class SearchrecordComponent implements OnInit {
     { id: 2, name: 'karafjka', surname: 'kowalska', postcode: '50-235', city: 'Wroclaw', street: 'wawrzynkowskiego', number: 20, flatNumber: 90},
     { id: 8, name: 'anna', surname: 'towalska', postcode: '60-235', city: 'Krakow', street: 'pilsudzkiego', number: 14, flatNumber: 12 },
     { id: 3, name: 'snna', surname: 'powalska', postcode: '70-235', city: 'Antonin', street: 'lukasinskiego', number: 8, flatNumber: 43 },
-    { id: 2, name: 'karafjka', surname: 'kowaelska', postcode: '50-245', city: 'Wroclaw', street: 'wawrzynkowskiego', number: 20, flatNumber: 90},
+    { id: 2, name: 'trafjka', surname: 'kowaelska', postcode: '50-245', city: 'Wroclaw', street: 'wawrzynkowskiego', number: 20, flatNumber: 90},
     { id: 89, name: 'asnna', surname: 'towalseka', postcode: '60-245', city: 'Krakow', street: 'pilsudzriego', number: 14, flatNumber: 12 },
     { id: 36, name: 'snsna', surname: 'powalseka', postcode: '70-245', city: 'Antorin', street: 'lukasirskiego', number: 8, flatNumber: 43 },
     { id: 25, name: 'karsafjka', surname: 'kowealska', postcode: '50-245', city: 'Wgoclaw', street: 'warrzynkowskiego', number: 20, flatNumber: 90},
@@ -35,7 +36,7 @@ export class SearchrecordComponent implements OnInit {
     { id: 34, name: 'snnsa', surname: 'peowalska', postcode: '73-235', city: 'Anhtonin', street: 'lukasrnskiego', number: 8, flatNumber: 43 },
     { id: 2, name: 'karafjka', surname: 'kowalska', postcode: '50-235', city: 'Wroclaw', street: 'wawrzynkowskiego', number: 20, flatNumber: 90},
     { id: 8, name: 'anna', surname: 'towalska', postcode: '60-235', city: 'Krakow', street: 'pilsudzkiego', number: 14, flatNumber: 12 },
-    { id: 3, name: 'snna', surname: 'powalska', postcode: '70-235', city: 'Antonin', street: 'lukasinskiego', number: 8, flatNumber: 43 },
+    { id: 3, name: 'snna', surname: 'aowalska', postcode: '70-235', city: 'Antonin', street: 'lukasinskiego', number: 8, flatNumber: 43 },
     { id: 2, name: 'karafjka', surname: 'kowaelska', postcode: '50-245', city: 'Wroclaw', street: 'wawrzynkowskiego', number: 20, flatNumber: 90},
     { id: 89, name: 'asnna', surname: 'towalseka', postcode: '60-245', city: 'Krakow', street: 'pilsudzriego', number: 14, flatNumber: 12 },
     { id: 36, name: 'snsna', surname: 'powalseka', postcode: '70-245', city: 'Antorin', street: 'lukasirskiego', number: 8, flatNumber: 43 },
@@ -43,11 +44,19 @@ export class SearchrecordComponent implements OnInit {
     { id: 82, name: 'annsa', surname: 'towalskea', postcode: '60-245', city: 'Krasow', street: 'pilsudzriego', number: 14, flatNumber: 12 },
     { id: 32, name: 'snsna', surname: 'powalskea', postcode: '73-235', city: 'Antosin', street: 'lukasirskiego', number: 8, flatNumber: 43 },
     { id: 23, name: 'karsafjka', surname: 'kowealska', postcode: '53-235', city: 'Wsoclaw', street: 'warrzynkowskiego', number: 20, flatNumber: 90},
-    { id: 85, name: 'annsa', surname: 'towealska', postcode: '60-235', city: 'Kradkow', street: 'pilsudrkiego', number: 14, flatNumber: 12 },
-    { id: 34, name: 'snnsa', surname: 'peowalska', postcode: '73-235', city: 'Anhtonin', street: 'lukasrnskiego', number: 8, flatNumber: 43 },
+    { id: 85, name: 'znnsa', surname: 'aowealska', postcode: '60-235', city: 'Kradkow', street: 'pilsudrkiego', number: 14, flatNumber: 12 },
+    { id: 34, name: 'znnsa', surname: 'zeowalska', postcode: '13-235', city: 'Anhtonin', street: 'lukasrnskiego', number: 8, flatNumber: 43 },
   ];
 
   public searchString: string;
+  id_sorted = false;
+  name_sorted = false;
+  surname_sorted = false;
+  postcode_sorted = false;
+  city_sorted = false;
+  street_sorted = false;
+  number_sorted = false;
+  flat_number_sorted = false;
 
   constructor(
     public router: Router
@@ -57,57 +66,122 @@ export class SearchrecordComponent implements OnInit {
   ngOnInit() {
   }
 
-  sortById() {
-    this.users.sort((a, b) => a.id - b.id);
-    // console.log('clicked');
+  sortById(){
+    if (this.id_sorted === false) {
+      this.users.sort((a, b) => a.id - b.id);
+      this.id_sorted = true;
+    }
+    else {
+      this.users.sort((a, b) => b.id - a.id);
+      this.id_sorted = false;
+    }
   }
 
- /* revertSortById(){
-    this.users.sort((a, b) => b.id - a.id);
-    console.log('revert-clicked');
-  }*/
-
   sortByName() {
-    this.users.sort((a, b) => { if (a.name < b.name) { return -1; }
-      if (a.name > b.name) {return 1; }
-      return 0;
-    } );
+    if (this.name_sorted === false) {
+      this.users.sort((a, b) => { if (a.name < b.name) { return -1; }
+        if (a.name > b.name) {return 1; }
+        return 0;
+      } );
+      this.name_sorted = true;
+    }
+    else {
+      this.users.sort((a, b) => { if (a.name > b.name) { return -1; }
+        if (a.name < b.name) {return 1; }
+        return 0;
+      } );
+      this.name_sorted = false;
+    }
   }
 
   sortBySurname() {
-    this.users.sort((a, b) => { if (a.surname < b.surname) { return -1; }
-      if (a.surname > b.surname) {return 1; }
-      return 0;
-    } );
+    if (this.surname_sorted === false) {
+      this.users.sort((a, b) => { if (a.surname < b.surname) { return -1; }
+        if (a.surname > b.surname) {return 1; }
+        return 0;
+      } );
+      this.surname_sorted = true;
+    }
+    else {
+      this.users.sort((a, b) => { if (a.surname < b.surname) { return 1; }
+        if (a.surname > b.surname) {return -1; }
+        return 0;
+      } );
+      this.surname_sorted = false;
+    }
   }
 
   sortByPostcode() {
-    this.users.sort((a, b) => { if (a.postcode < b.postcode) { return -1; }
-      if (a.postcode > b.postcode) {return 1; }
-      return 0;
-    } );
+    if (this.postcode_sorted === false) {
+      this.users.sort((a, b) => { if (a.postcode < b.postcode) { return -1; }
+        if (a.postcode > b.postcode) {return 1; }
+        return 0;
+      } );
+      this.postcode_sorted = true;
+    }
+    else {
+      this.users.sort((a, b) => { if (a.postcode < b.postcode) { return 1; }
+        if (a.postcode > b.postcode) {return -1; }
+        return 0;
+      } );
+      this.postcode_sorted = false;
+    }
   }
 
   sortByCity() {
-    this.users.sort((a, b) => { if (a.city < b.city) { return -1; }
-      if (a.city > b.city) {return 1; }
-      return 0;
-    } );
+    if (this.city_sorted === false) {
+      this.users.sort((a, b) => { if (a.city < b.city) { return -1; }
+        if (a.city > b.city) {return 1; }
+        return 0;
+      } );
+      this.city_sorted = true;
+    }
+    else {
+      this.users.sort((a, b) => { if (a.city < b.city) { return 1; }
+        if (a.city > b.city) {return -1; }
+        return 0;
+      } );
+      this.city_sorted = false;
+    }
   }
 
   sortByStreet() {
-    this.users.sort((a, b) => { if (a.street < b.street) { return -1; }
-      if (a.street > b.street) {return 1; }
-      return 0;
-    } );
+    if (this.street_sorted === false) {
+      this.users.sort((a, b) => { if (a.street < b.street) { return -1; }
+        if (a.street > b.street) {return 1; }
+        return 0;
+      } );
+      this.street_sorted = true;
+    }
+    else {
+      this.users.sort((a, b) => { if (a.street < b.street) { return 1; }
+        if (a.street > b.street) {return -1; }
+        return 0;
+      } );
+      this.street_sorted = false;
+    }
   }
 
   sortByNumber() {
-    this.users.sort((a, b) => a.number - b.number);
+    if (this.number_sorted === false) {
+      this.users.sort((a, b) => a.number - b.number);
+      this.number_sorted = true;
+    }
+    else {
+      this.users.sort((a, b) => b.number - a.number);
+      this.number_sorted = false;
+    }
   }
 
   sortByFlatNumber() {
-    this.users.sort((a, b) => a.flatNumber - b.flatNumber);
+    if (this.flat_number_sorted === false) {
+      this.users.sort((a, b) => a.flatNumber - b.flatNumber);
+      this.flat_number_sorted = true;
+    }
+    else {
+      this.users.sort((a, b) => b.flatNumber - a.flatNumber);
+      this.flat_number_sorted = false;
+    }
   }
 
   search() {
@@ -120,6 +194,8 @@ export class SearchrecordComponent implements OnInit {
   navigate(route: string) {
   this.router.navigate([route]);
   }
+
+
 }
 
 
